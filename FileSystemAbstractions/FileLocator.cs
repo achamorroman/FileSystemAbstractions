fbd79040c4101e9d567fd48e52a6ae139e7d2f62
Antonio Chamorro <achamorroman@outlook.com>
2015-03-27 11:16:04 +0100

    Initial commit

---
+++ FileSystemAbstractions/FileLocator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemAbstractions
+{
+    public class FileLocator
+    {
+        private readonly string _searchDirectory;
+        private readonly string _searchPattern;
+        private readonly string _fileExtension;
+
+        public FileLocator(String searchDirectory, string searchPattern, string fileExtension)
+        {
+            if (String.IsNullOrEmpty(searchDirectory)) throw new ArgumentNullException("searchDirectory");
+            if (string.IsNullOrEmpty(searchPattern)) throw new ArgumentException("searchString");
+            if (string.IsNullOrEmpty(fileExtension)) throw new ArgumentException("fileExtension");
+
+            if (!Directory.Exists(searchDirectory))
+                throw new ArgumentException("Root folder doesn't exists", "searchDirectory");
+
+            _searchDirectory = Path.GetFullPath(searchDirectory);
+            _searchPattern = searchPattern;
+            _fileExtension = fileExtension;
+        }
+
+        public IEnumerable<MyFile> GetSearchResult()
+        {
+            var searchDirectoryInfo = new DirectoryInfo(_searchDirectory);
+            var matchingFiles = GetMatchingFiles(searchDirectoryInfo);
+
+            if (!matchingFiles.Any()) return new List<MyFile>();
+
+            var files = new List<MyFile>();
+
+            foreach (var file in matchingFiles)
+            {
+                files.Add(new MyFile() { FileName = file.Name, DirectoryName = file.DirectoryName });
+            }
+            return files;
+        }
+
+        private IEnumerable<FileInfo> GetMatchingFiles(DirectoryInfo viewsRootDirectoryInfo)
+        {
+            var fileList = viewsRootDirectoryInfo.GetFiles(_searchPattern, SearchOption.AllDirectories);
+            var matchingFiles = from file in fileList
+                                where file.Extension == _fileExtension
+                                select file;
+
+            return matchingFiles;
+        }
+    }
+
+    public class MyFile
+    {
+        public String FileName { get; set; }
+        public String DirectoryName { get; set; }
+    }
+
+}
---
+++ FileSystemAbstractions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemAbstractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLocator = new FileLocator(@"D:\Ocio", "The*", ".avi");
+            var files = fileLocator.GetSearchResult();
+
+            foreach (var file in files)
+            {
+                Debug.Print("Fichero: {0} en directorio: {1}", file.FileName, file.DirectoryName);
+            }
+        }
+    }
+}
