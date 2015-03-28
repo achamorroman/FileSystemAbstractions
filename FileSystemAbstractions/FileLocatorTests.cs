7afb13077268d513531b41e6119fa8928bfb15f4
Antonio Chamorro <achamorroman@outlook.com>
2015-03-28 12:33:28 +0100

    Tests con un searchPattern distinto a *.*

--- FileSystemAbstractions/FileLocatorTests.cs
+++ FileSystemAbstractions/FileLocatorTests.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace FileSystemAbstractions
 {
     public class FileLocatorTests
     {
-
         private const string AllFilesSearchPattern = "*.*";
+        private const string RootFolder = @"C:\root";
         private IFileSystem _mockedFileSystem;
 
         [TestFixtureSetUp]
@@ -22,51 +19,17 @@ namespace FileSystemAbstractions
         }
 
         [TestFixtureTearDown]
-        public void TearDown() { }
-
-        private static IFileSystem GetMockedFileSystem()
+        public void TearDown()
         {
-            return new MockFileSystem(new Dictionary<string, MockFileData>
-                {
-                    { @"c:\myfile.txt", new MockFileData("Testing is meh.") },
-                    { @"c:\demo\jQuery.js", new MockFileData("some js") },
-                    { @"c:\demo\document.doc", new MockFileData("document text") },
-                    { @"c:\demo\ExcelSheet.xls", new MockFileData("excel data") },
-                    { @"c:\demo\otherDemo\MyOwn.js", new MockFileData("more js") }
-                });
-        }
-
-        [Test]
-        [ExpectedException("System.ArgumentException")]
-        public void When_rootFolderNotExists_Expected_ArgumentException()
-        {
-            //Arrange
-            const string notExistingDirectory = "C:\notExistingDirectory";
-
-            //Act - Assert
-            var sut = new FileLocator(_mockedFileSystem, notExistingDirectory, AllFilesSearchPattern, ".txt");
-        }
-
-        [Test]
-        public void When_rootFolderExists_Expected_NewFileLocator()
-        {
-            //Arrange
-            const string searchRootFolder = @"C:\Demo";
-
-            //Act
-            var sut = new FileLocator(_mockedFileSystem, searchRootFolder, AllFilesSearchPattern, ".txt");
-
-            //Assert
-            Assert.IsNotNull(sut);
         }
 
         [Test]
         public void When_FilesFound_Expected_ResultsWithMyFileInfo()
         {
             //Arrange
-            const string searchRootFolder = @"C:\demo";
-            var sut = new FileLocator(_mockedFileSystem, searchRootFolder, AllFilesSearchPattern, ".js");
+            const string fileExtension = ".js";
             const int expectedFiles = 2;
+            var sut = new FileLocator(_mockedFileSystem, RootFolder, AllFilesSearchPattern, fileExtension);
 
             //Act
             var files = sut.GetSearchResult();
@@ -77,17 +40,75 @@ namespace FileSystemAbstractions
         }
 
         [Test]
-        public void When_FilesNotFound_Expected_EmptyResults()
+        public void When_SearchPatternProvidedAndExistsCoincidence_Expected_ResultsWithMyFileInfo()
         {
             //Arrange
-            const string searchRootFolder = @"C:\demo";
-            var sut = new FileLocator(_mockedFileSystem, searchRootFolder, AllFilesSearchPattern, ".dat");
+            const string fileExtension = ".doc";
+            const string searchPattern = "*doc*";
+            const int expectedFiles = 2;
+
+            var sut = new FileLocator(_mockedFileSystem, RootFolder, searchPattern, fileExtension);
 
             //Act
             var files = sut.GetSearchResult();
 
             //Assert
+            Assert.AreEqual(expectedFiles, files.Count());
+            Assert.IsInstanceOf<MyFileInfo>(files.First());
+        }
+
+
+        [Test]
+        public void When_FilesNotFound_Expected_EmptyResults()
+        {
+            //Arrange
+            const string fileExtension = ".dat";
+            var sut = new FileLocator(_mockedFileSystem, RootFolder, AllFilesSearchPattern, fileExtension);
+
+            //Act
+            IEnumerable<MyFileInfo> files = sut.GetSearchResult();
+
+            //Assert
             Assert.IsEmpty(files);
+        }
+
+        [Test]
+        public void When_rootFolderExists_Expected_NewFileLocator()
+        {
+            //Arrange
+            const string fileExtension = ".txt";
+
+            //Act
+            var sut = new FileLocator(_mockedFileSystem, RootFolder, AllFilesSearchPattern, fileExtension);
+
+            //Assert
+            Assert.IsNotNull(sut);
+            Assert.IsInstanceOf<FileLocator>(sut);
+        }
+
+        [Test]
+        [ExpectedException("System.ArgumentException")]
+        public void When_rootFolderNotExists_Expected_ArgumentException()
+        {
+            //Arrange
+            const string notExistingFolder = "C:\notExistingFolder";
+            const string fileExtension = ".txt";
+
+            //Act - Assert
+            var sut = new FileLocator(_mockedFileSystem, notExistingFolder, AllFilesSearchPattern, fileExtension);
+        }
+
+        private static IFileSystem GetMockedFileSystem()
+        {
+            return new MockFileSystem(new Dictionary<string, MockFileData>
+                                      {
+                                          {@"c:\myfile.txt", new MockFileData("Testing is meh.")},
+                                          {@"c:\root\jQuery.js", new MockFileData("some js")},
+                                          {@"c:\root\document.doc", new MockFileData("document text")},
+                                          {@"c:\root\excelSheet.xls", new MockFileData("excel data")},
+                                          {@"c:\root\folder1\myDoc.js", new MockFileData("more js")},
+                                          {@"c:\root\folder2\otherdoc.doc", new MockFileData("more js")}
+                                      });
         }
     }
 }
